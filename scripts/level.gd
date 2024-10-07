a96cee3724a631b8048a4f1b091a78851f66e399
Logodum <15616120+PavelProvotorov@users.noreply.github.com>
2024-10-07 17:30:06 +0300

    Level variables - Added automatic detection of map width and height - Added TILES_LOGIC variable holding the id's of cells in logic tilemap layer

--- scripts/level.gd
+++ scripts/level.gd
@@ -5,6 +5,15 @@ onready var DEBUG_LABEL = preload("res://scenes/DebugLabel.tscn")
 onready var TILEMAP_LOGIC = $Logic
 onready var TILEMAP_DEBUG = $Debug
 
+onready var MAP_WIDTH = TILEMAP_LOGIC.get_used_rect().size.x
+onready var MAP_HEIGHT = TILEMAP_LOGIC.get_used_rect().size.y
+
+onready var TILES_LOGIC = {
+	FLOOR = TILEMAP_LOGIC.tile_set.find_tile_by_name("TILE_FLOOR"),
+	WALL = TILEMAP_LOGIC.tile_set.find_tile_by_name("TILE_WALL"),
+	EMPTY = TILEMAP_LOGIC.tile_set.find_tile_by_name("TILE_EMPTY")
+	}
+
 func _ready():
 	debug_add_cell_positions(TILEMAP_LOGIC.get_used_cells())
 	pass
