a7ca9bc5559dfd150e483a75a757c6638101e8df
Pavel Provotorov <pavel.provotorov@veriff.net>
2026-01-03 22:52:31 +0200

    Changed color palette and buffed hydra

--- scripts/mobs/hydra.gd
+++ scripts/mobs/hydra.gd
@@ -2,6 +2,8 @@ extends Enemy2D
 
 func _ready():
 	behaviours = [
+		BEHAVIOUR_TYPE.FLEE,
+		BEHAVIOUR_TYPE.AMBUSH,
 		BEHAVIOUR_TYPE.RANGED,
 		BEHAVIOUR_TYPE.MELEE,
 		BEHAVIOUR_TYPE.MOVE,
@@ -18,3 +20,10 @@ func _post_ranged_attack_hook():
 
 func _post_melee_attack_hook():
 	if get_chance(25): target.add_buff('poison', 6)
+	
+func get_flee_behaviour_config() -> Dictionary:
+	return {
+		"health_threshold": 0,
+		"flee_when_close": true,
+		"skip_chance": 25,
+	}
